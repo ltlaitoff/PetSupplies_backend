8c44680b1f8dd8641b9937ea01cb1590c82a9a5f
ltlaitoff <ltlaitoff@gmail.com>
2022-11-26 19:59:51 +0200

    feat: Created Card model

--- src/models/Card.ts
+++ src/models/Card.ts
@@ -1,13 +1,56 @@
 import { Schema, model } from 'mongoose'
 
 interface CardType {
-	id: number
 	title: string
+	description: string
+	type: 'Cats' | 'Dogs'
+	price: number
+	stars: number
+	image: Buffer
+	category: string
+	producer: string
+	novelty?: boolean
+	discount?: boolean
+	discountValue?: number
+	creator: string
 }
 
-const cardSchema = new Schema<CardType>({
-	id: { type: Number, required: true },
-	title: { type: String, required: true },
+const cardSchema = new Schema<CardType>(
+	{
+		title: { type: String, required: true },
+		description: { type: String, required: true },
+		type: { type: String, required: true },
+		price: { type: Number, required: true },
+		stars: { type: Number, required: true },
+		image: { type: Buffer, required: true, contentType: String },
+		category: { type: String, required: true },
+		producer: { type: String, required: true },
+		novelty: { type: Boolean, required: true, default: false },
+		discount: { type: Boolean, required: true, default: false },
+		discountValue: { type: Number, required: true, default: 0 },
+		creator: { type: String, required: true, default: '-' },
+	},
+	{
+		timestamps: true,
+	}
+)
+
+cardSchema.pre('save', function (next) {
+	if (this.type !== 'Dogs' && this.type !== 'Cats') {
+		throw new Error('Not valid card type')
+	}
+
+	if (this.price < 0) {
+		throw new Error('Not valid card price')
+	}
+
+	if (this.stars < 0 || this.stars > 5) {
+		throw new Error('Not valid card stars')
+	}
+
+	// TODO: Save images
+
+	next()
 })
 
 const Card = model<CardType>('Card', cardSchema)
