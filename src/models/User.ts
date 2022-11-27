46ed42602fb3e82cbf1b10d5d1fb247d8dedc45e
ltlaitoff <ltlaitoff@gmail.com>
2022-11-27 18:20:27 +0200

    feat: Created User model

---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, model } from 'mongoose'
+import { allFieldsRequiredByDefault } from '../helpers/allFieldRequiredByDefault'
+import { AccountLevel } from './AccountLevel'
+
+const userSchema = new Schema(
+	{
+		surname: String,
+		name: String,
+		email: String,
+		phone: String,
+		password: String,
+		country: String,
+		city: String,
+		zipCode: Number,
+		address: String,
+		accountAdminLevel: { type: Schema.Types.ObjectId, ref: AccountLevel },
+	},
+	{
+		timestamps: true,
+	}
+)
+
+allFieldsRequiredByDefault(userSchema)
+
+const User = model('User', userSchema)
+
+export { User }
