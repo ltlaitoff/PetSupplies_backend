d4c1c2f9a00abd891fcd417071c91600c7563336
ltlaitoff <ltlaitoff@gmail.com>
2022-11-27 18:27:10 +0200

    feat: Created Product model

---
+++ src/models/Product.ts
@@ -0,0 +1,50 @@
+import { Schema, model } from 'mongoose'
+import { allFieldsRequiredByDefault } from '../helpers/allFieldRequiredByDefault'
+
+import { Category, Producer, ProductsAllInfo, Type, User } from '.'
+
+const productSchema = new Schema(
+	{
+		title: String,
+		description: String,
+		price: Number,
+		stars: Number,
+		image: { type: Buffer, contentType: String },
+		type: { type: [Schema.Types.ObjectId], ref: Type },
+		category: { type: [Schema.Types.ObjectId], ref: Category },
+		producer: { type: Schema.Types.ObjectId, ref: Producer },
+		novelty: { type: Boolean, default: false },
+		discount: { type: Boolean, default: false },
+		discountValue: { type: Number, default: -1 },
+		discountPreviousValue: { type: Number, default: -1 },
+		allInfo: { type: Schema.Types.ObjectId, ref: ProductsAllInfo },
+		creator: { type: Schema.Types.ObjectId, ref: User },
+	},
+	{
+		timestamps: true,
+	}
+)
+
+allFieldsRequiredByDefault(productSchema)
+
+const Product = model('Product', productSchema)
+
+export { Product }
+
+// cardSchema.pre('save', function (next) {
+// 	if (this.type !== 'Dogs' && this.type !== 'Cats') {
+// 		throw new Error('Not valid card type')
+// 	}
+
+// 	if (this.price < 0) {
+// 		throw new Error('Not valid card price')
+// 	}
+
+// 	if (this.stars < 0 || this.stars > 5) {
+// 		throw new Error('Not valid card stars')
+// 	}
+
+// 	// TODO: Save images
+
+// 	next()
+// })
